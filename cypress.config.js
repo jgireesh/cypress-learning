18719335a022acd0698779aee575dbb4cf2da22a
jgireesh <gireeshajeera@gmail.com>
2023-02-15 13:18:18 +0000

    allure reports folder strucure added, allure dependencies-plugins-configs changed

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,21 @@
 const { defineConfig } = require("cypress");
 
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
+// var webpackPreprocessor = require('webpack');
+
 module.exports = defineConfig({
   e2e: {
     googleUrl:'http://www.google.com',
     setupNodeEvents(on, config) {
-      
+      //implement node event listeners here
+      // on('file:preprocessor', webpackPreprocessor)
+      allureWriter(on, config);
+      return config;
     },
+    env: {
+      allureReuseAfterSpec: true
+  },
     specPattern:'cypress/e2e/qa_project/*.js'
   },
 });
